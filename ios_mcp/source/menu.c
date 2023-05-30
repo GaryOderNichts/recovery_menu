75b95141ebc738d40e8b3f90369bca8ee310be48
Thomas Rohloff <v10lator@myway.de>
2023-05-30 14:19:05 +0200

    Allow batch WUP install
    Signed-off-by: Thomas Rohloff <v10lator@myway.de>

--- ios_mcp/source/menu.c
+++ ios_mcp/source/menu.c
@@ -35,12 +35,12 @@
 #include "DebugSystemRegion.h"
 #include "SystemInformation.h"
 #include "SubmitSystemData.h"
+#include "Installer.h"
 
 static void option_SetColdbootTitle(void);
 static void option_DumpSyslogs(void);
 static void option_DumpOtpAndSeeprom(void);
 static void option_LoadNetConf(void);
-static void option_InstallWUP(void);
 static void option_EditParental(void);
 static void option_Shutdown(void);
 
@@ -709,79 +709,6 @@ static void option_LoadNetConf(void)
 
     IOS_HeapFree(CROSS_PROCESS_HEAP_ID, cfgBuffer);
     FSA_CloseFile(fsaHandle, cfgHandle);
-}
-
-static void option_InstallWUP(void)
-{
-    gfx_clear(COLOR_BACKGROUND);
-    drawTopBar("Installing WUP");
-
-    uint32_t index = 16 + 8 + 2 + 8;
-    gfx_print(16, index, 0, "Make sure to place a valid signed WUP directly in 'sd:/install'");
-    index += CHAR_SIZE_DRC_Y + 4;
-
-    int mcpHandle = IOS_Open("/dev/mcp", 0);
-    if (mcpHandle < 0) {
-        gfx_set_font_color(COLOR_ERROR);
-        gfx_printf(16, index, 0, "Failed to open /dev/mcp: %x", mcpHandle);
-        waitButtonInput();
-        return;
-    }
-
-    gfx_print(16, index, 0, "Querying install info...");
-    index += CHAR_SIZE_DRC_Y + 4;
-
-    MCPInstallInfo info;
-    int res = MCP_InstallGetInfo(mcpHandle, "/vol/storage_recovsd/install", &info);
-    if (res < 0) {
-        gfx_set_font_color(COLOR_ERROR);
-        gfx_printf(16, index, 0, "Failed to get install info: %x", res);
-        waitButtonInput();
-
-        IOS_Close(mcpHandle);
-        return;
-    }
-
-    gfx_printf(16, index, 0, "Installing title: %08lx-%08lx...",
-        (uint32_t)(info.titleId >> 32), (uint32_t)(info.titleId & 0xFFFFFFFFU));
-    index += CHAR_SIZE_DRC_Y + 4;
-
-    // only install to NAND
-    res = MCP_InstallSetTargetDevice(mcpHandle, 0);
-    if (res < 0) {
-        gfx_set_font_color(COLOR_ERROR);
-        gfx_printf(16, index, 0, "MCP_InstallSetTargetDevice: %x", res);
-        waitButtonInput();
-
-        IOS_Close(mcpHandle);
-        return;
-    }
-    res = MCP_InstallSetTargetUsb(mcpHandle, 0);
-    if (res < 0) {
-        gfx_set_font_color(COLOR_ERROR);
-        gfx_printf(16, index, 0, "MCP_InstallSetTargetUsb: %x", res);
-        waitButtonInput();
-
-        IOS_Close(mcpHandle);
-        return;
-    }
-
-    // TODO: async installations
-    res = MCP_InstallTitle(mcpHandle, "/vol/storage_recovsd/install");
-    if (res < 0) {
-        gfx_set_font_color(COLOR_ERROR);
-        gfx_printf(16, index, 0, "Failed to install: %x", res);
-        waitButtonInput();
-
-        IOS_Close(mcpHandle);
-        return;
-    }
-
-    gfx_set_font_color(COLOR_SUCCESS);
-    gfx_print(16, index, 0, "Done!");
-    waitButtonInput();
-
-    IOS_Close(mcpHandle);
 }
 
 static void option_EditParental(void)
